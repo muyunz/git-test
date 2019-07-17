d622ee3756e4f11f4ac31c646350ddd0596e08e4
BaMuYun <breadburn@gmail.com>
2019-07-17 21:10:58 +0800

    1

--- button.sh
+++ button.sh
@@ -1,7 +1,12 @@
 for i in {1..100}
 do
+   git branch "b-$i"
+   git checkout "b-$i"
    echo "$i" > data
    git add -A
    git commit -am "$i"
+   git checkout master
+   git merge "b-$i"
+   git branch -d "b-$i"
    git push -u origin master
 done
